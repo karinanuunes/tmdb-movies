2a1067473557b3011c727cd833f64fda64b6a16b
Karina Nunes <karina.nuunes@yahoo.com.br>
2024-06-20 23:56:58 -0300

    feat: ensure link to homepage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import logoTmdb from "../assets/logo-tmdb.png";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="w-full bg-[#5C16C5] py-4 flex justify-center lg:justify-start px-28">
+    <Link
+      href={"/"}
+      className="w-full bg-[#5C16C5] py-4 flex justify-center lg:justify-start px-28"
+    >
       <Image src={logoTmdb} width={185} height={24} alt="Logo da TMDB" />
-    </div>
+    </Link>
   );
 };
 
